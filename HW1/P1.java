4ba5d21bbd36e2aadac17663d00008c823cdd530
Jongseo Lee <leejseo@kaist.ac.kr>
2019-12-25 12:34:40 +0900

    Create P1.java

---
+++ HW1/P1.java
@@ -0,0 +1,18 @@
+package elice;
+
+import java.io.*;
+import java.util.Scanner;
+import elice.EliceUtils;
+
+public class Main {
+	public static void main(String args[]) {
+		//implement here
+        Scanner input = new Scanner(System.in);
+        
+        String str = input.nextLine();
+        
+        System.out.print("Hi, "); //Given for the problem 1
+        System.out.println(str); //Do not modify the part of the code given in this line.
+        System.out.println("How are you?"); //Given for the problem 1
+	}
+}
